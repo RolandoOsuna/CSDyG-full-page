1a433f357571544182f9d4bbbf830f46becbce2c
Rolando Osuna Aguirre <rolando_osuna@live.com.mx>
2022-05-25 22:00:23 -0600

    header scroll para abajo

--- JS/main.js
+++ JS/main.js
@@ -27,29 +27,60 @@ carrusel.addEventListener("mouseout", () => {
 });
 
 start();
-
+// CLICK
 let titulo = document.querySelector('.titulo-esquema');
 let conten = document.querySelector('.conten-esquema');
 titulo.addEventListener('click', element => {
   conten.classList.toggle('transicion-esquema')
-})
+});
 let titulo2 = document.querySelector('.titulo-esquema2');
 let conten2 = document.querySelector('.conten-esquema2');
 titulo2.addEventListener('click', element => {
   conten2.classList.toggle('transicion-esquema2')
-})
+});
 let titulo3 = document.querySelector('.titulo-esquema3');
 let conten3 = document.querySelector('.conten-esquema3');
 titulo3.addEventListener('click', element => {
   conten3.classList.toggle('transicion-esquema3')
-})
+});
 let titulo4 = document.querySelector('.titulo-esquema4');
 let conten4 = document.querySelector('.conten-esquema4');
 titulo4.addEventListener('click', element => {
   conten4.classList.toggle('transicion-esquema4')
-})
+});
 let titulo5 = document.querySelector('.titulo-esquema5');
 let conten5 = document.querySelector('.conten-esquema5');
 titulo5.addEventListener('click', element => {
   conten5.classList.toggle('transicion-esquema5')
-})
+});
+// TRANSICION SCROLL
+const planeacion = document.getElementById('planeacion');
+const alcance = document.getElementById('alcance');
+const enfoque = document.getElementById('enfoque');
+const organizacion = document.getElementById('organizacion');
+const crecimiento = document.getElementById('crecimiento');
+
+const cargarContenido = (entradas, observador) => {
+	// console.log(entradas)
+	// console.log(observador)
+
+	entradas.forEach((entrada) => {
+		if(entrada.isIntersecting){
+			entrada.target.classList.add('visibleee');
+		} else {
+			// entrada.target.classList.remove('visible');
+		}
+	});
+}
+
+const observador = new IntersectionObserver(cargarContenido, {
+	root: null,
+	rootMargin: '500px 0px 100px 0px',
+	threshold: 1.0
+});
+
+observador.observe(planeacion);
+observador.observe(alcance);
+observador.observe(enfoque);
+observador.observe(organizacion);
+observador.observe(crecimiento);
